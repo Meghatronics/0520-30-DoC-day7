f5db88b305d47aeea43a35737d77a0871c349c6d
MajorE <dolea_exe@outlook.com>
2020-05-08 00:29:04 +0100

    Flutter default counter app implemented using Provider

---
+++ android/app/src/main/kotlin/com/example/day7_30doc/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.day7_30doc
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider<Increment>(
+      create: (BuildContext context) => Increment(),
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: MyHomePage(),
+      ),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Title'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              Provider.of<Increment>(context).data.toString(),
+              style: Theme.of(context).textTheme.headline4,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Provider.of<Increment>(context, listen: false).increaseNumber();
+        },
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
+
+class Increment extends ChangeNotifier {
+  int data = 0;
+
+  void increaseNumber() {
+    data++;
+    notifyListeners();
+  }
+}
